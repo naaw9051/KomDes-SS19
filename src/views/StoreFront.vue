457cf12c7d90556b5cac10236b0884740530a627
Nathalie <n.awitor@gmx.de>
2019-07-07 01:21:00 +0200

    Add markup for page 2

--- src/views/StoreFront.vue
+++ src/views/StoreFront.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="page">
-    <h1>Content für Seite 2</h1>
-    <div class="text">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis neque ad delectus itaque fugiat distinctio consequatur aliquam quo quibusdam illum. Non enim vero corrupti, aliquam sit facere maxime. Voluptatum, tempora.
-      </p>    
+    <div class="background">
+      <div class="background__info">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis neque ad delectus itaque fugiat distinctio consequatur aliquam quo quibusdam illum. Non enim vero corrupti, aliquam sit facere maxime. Voluptatum, tempora.
+        </p>
+      </div>
+    </div>
+    <div class="foreground">
     </div>
   </div>
 </template>
@@ -16,5 +19,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.page {
+  width: 100%;
+  height: 100%;
+  background: url('../assets/images/background.jpg') 0 0 no-repeat;
 
+  .background {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left:0;
+    top: 0;
+
+    &__info {
+      position: absolute;
+      left: 50%;
+      top: 30%;
+      transform: translate(-50%, -50%);
+      max-width: 550px;
+      color: #afafaf;
+      font-size: 20px;
+      filter: drop-shadow(0 0 50px #ffffff);
+    }
+  }
+
+ .foreground {
+    position: relative;
+    left: 55%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    height: 500px;
+    width: 600px;
+  }
+}
 </style>
